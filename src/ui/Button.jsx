4e9e0ebc271cf67fe263c021f15e928316dc2617
DominikKorpusik <dominik.korpusik@gmail.com>
2024-03-23 20:18:12 +0100

    UPDATE small button styles in Button.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,7 +7,7 @@ function Button({ children, disabled, to, type, onClick }) {
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
-    small: base + " py-2 md:px-5 md:py-2.5 text-xs",
+    small: base + " py-2 px-2 md:px-5 md:py-2.5 text-xs",
     round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "inline-block text-sm  rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-500 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 focus:text-stone-500 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
