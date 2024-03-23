3e0d25c5cb143cfbf819616799c327dc139c6b2d
DominikKorpusik <dominik.korpusik@gmail.com>
2024-03-23 11:33:45 +0100

    ADD username to components.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -36,6 +37,7 @@ const fakeCart = [
 function CreateOrder() {
   const naviagtion = useNavigation();
   const isSubmitting = naviagtion.state === "submitting";
+  const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
 
@@ -52,7 +54,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" className="input grow" required />
+          <input
+            type="text"
+            name="customer"
+            defaultValue={username}
+            className="input grow"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
