d1856bb01871f11a8324f070fd86422dd8e25a16
DominikKorpusik <dominik.korpusik@gmail.com>
2024-03-23 11:19:05 +0100

    CREATE userSlice

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="text-sm font-semibold md:block">Dominik K.</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return <div className="text-sm font-semibold md:block">{username}</div>;
 }
 
 export default Username;
